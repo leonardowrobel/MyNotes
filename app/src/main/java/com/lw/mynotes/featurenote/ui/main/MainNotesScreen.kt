dc5e6dc348edc3ea3c1183bbcb6befa152957ddf
Leonardo <leonardo.wrobel@gmail.com>
2026-01-25 20:49:28 -0300

    wip(): add note button;

--- app/src/main/java/com/lw/mynotes/featurenote/ui/main/MainNotesScreen.kt
+++ app/src/main/java/com/lw/mynotes/featurenote/ui/main/MainNotesScreen.kt
@@ -1,8 +1,11 @@
 package com.lw.mynotes.featurenote.ui.main
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -10,8 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.lw.mynotes.featurenote.ui.components.Fab
 import com.lw.mynotes.featurenote.ui.util.NavigationItem
 
 @Composable
@@ -22,17 +27,30 @@ fun MainNotesScreen(
     Surface(
         Modifier.fillMaxSize(), color = Color.LightGray
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(MainNotesViewModel.TAG)
-            Button(onClick = {
-                navController.navigate(NavigationItem.AddEditNote.route)
-            }) {
-                Text("AddEditNoteScreen")
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ){
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(MainNotesViewModel.TAG)
+                Button(onClick = {
+                    navController.navigate(NavigationItem.AddEditNote.route)
+                }) {
+                    Text("AddEditNoteScreen")
+                }
             }
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(22.dp)
+            ){
+                Fab(
+                    modifier = Modifier.align(Alignment.BottomEnd),
+                    onClick = { navController.navigate(NavigationItem.AddEditNote.route) })
+            }
+
         }
     }
 }
