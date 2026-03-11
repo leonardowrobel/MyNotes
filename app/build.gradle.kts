3df11806329392216de2047c15f3e5d05da438cb
Leonardo <leonardo.wrobel@gmail.com>
2026-03-11 18:38:26 -0300

    up_version(0.5.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         minSdk = 34
         targetSdk = 35
 
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
