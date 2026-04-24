614a3d83fa103d6fd375294391c429cbc23ac596
Leonardo <leonardo.wrobel@gmail.com>
2026-04-24 11:04:17 -0300

    wip(): comment;

--- app/src/androidTest/java/com/lw/mynotes/featurenote/data/repository/FirebaseNoteRepositoryTest.kt
+++ app/src/androidTest/java/com/lw/mynotes/featurenote/data/repository/FirebaseNoteRepositoryTest.kt
@@ -1,31 +1,5 @@
 package com.lw.mynotes.featurenote.data.repository
 
-import android.util.Log
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.SmallTest
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.firestoreSettings
-import com.lw.mynotes.featurenote.data.data_source.dao.NotesDaoTest
-import com.lw.mynotes.featurenote.data.data_source.MyNotesDatabase
-import com.lw.mynotes.featurenote.data.data_source.dao.NotesDao
-import com.lw.mynotes.featurenote.data.model.NoteEntity
-import com.lw.mynotes.featurenote.data.repository.FirestoreNoteRepositoryImpl
-import com.lw.mynotes.featurenote.data.repository.NoteRepositoryImpl
-import com.lw.mynotes.featurenote.domain.model.Note
-import com.lw.mynotes.featurenote.domain.repository.FirestoreNoteRepository
-import com.lw.mynotes.featurenote.domain.repository.NoteRepository
-import kotlinx.coroutines.runBlocking
-import org.junit.After
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
-import java.io.FileReader
-import java.util.Date
-import java.util.Properties
-import javax.inject.Inject
-
 // TODO
 //@RunWith(AndroidJUnit4::class)
 //@SmallTest
