0f495fb5f6a8544ba2bd274a05d45cc492f7d896
Leonardo <leonardo.wrobel@gmail.com>
2026-05-05 11:02:01 -0300

    fix(): replace runTest for runBlocking;

--- app/src/androidTest/java/com/lw/mynotes/featurenote/data/data_source/dao/NotesDaoTest.kt
+++ app/src/androidTest/java/com/lw/mynotes/featurenote/data/data_source/dao/NotesDaoTest.kt
@@ -5,7 +5,7 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
-import com.lw.mynotes.featurenote.data.NotesTestingUtils
+import com.lw.mynotes.featurenote.NotesTestingUtils
 import com.lw.mynotes.featurenote.data.data_source.MyNotesDatabase
 import com.lw.mynotes.featurenote.data.model.NoteEntity
 import kotlinx.coroutines.runBlocking
--- app/src/test/java/com/lw/mynotes/featurenote/data/repository/NoteRepositoryImplTest.kt
+++ app/src/test/java/com/lw/mynotes/featurenote/data/repository/NoteRepositoryImplTest.kt
@@ -1,32 +1,47 @@
 package com.lw.mynotes.featurenote.data.repository
 
+import com.lw.mynotes.featurenote.NotesTestingUtils
 import com.lw.mynotes.featurenote.data.data_source.dao.NotesDao
 import com.lw.mynotes.featurenote.domain.repository.NotesRepository
+import io.mockk.MockKAnnotations
 import io.mockk.coEvery
-import io.mockk.mockk
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 
 // TODO(wip)
 class NoteRepositoryImplTest {
 
+    private val notesQtd = 10
+
+    private val notesTestingUtils = NotesTestingUtils()
+
     private lateinit var notesRepository: NotesRepository
-    private lateinit var notesDao: NotesDao
+    @MockK
+    lateinit var notesDao: NotesDao
 
     @Before
     fun setUp() {
-        notesDao = mockk<NotesDao>()
-        TODO("Not yet implemented")
-//        notesRepository = NotesRepositoryImpl()
+        MockKAnnotations.init(this)
+        notesRepository = NotesRepositoryImpl(notesDao)
     }
 
     @Test
     fun getAll() {
-//        coEvery { notesDao.getAll() } returns
+//        coEvery { notesDao.getAll() } returns notesTestingUtils.createNoteEntities(notesQtd)
     }
 
     @Test
     fun get() {
+        val mockNoteEntity = notesTestingUtils.createNoteEntityWithId()
+        coEvery { notesDao.get(ofType<Long>()) } returns mockNoteEntity
+
+        val actualNoteEntity = runBlocking {
+            notesRepository.get(mockNoteEntity.id)
+        }
+
+        assert(mockNoteEntity == actualNoteEntity)
     }
 
     @Test
@@ -40,5 +55,4 @@ class NoteRepositoryImplTest {
     @Test
     fun delete() {
     }
-
 }
