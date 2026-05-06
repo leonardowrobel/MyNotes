0c0c0405a3542a705001d8b4c6a0f07c8a0142c8
Leonardo <leonardo.wrobel@gmail.com>
2026-05-06 10:09:42 -0300

    wip(): change package name; cover some use cases;

---
+++ app/src/test/java/com/lw/mynotes/featurenote/services/NotesServiceTest.kt
@@ -0,0 +1,75 @@
+package com.lw.mynotes.featurenote.services
+
+import com.lw.mynotes.featurenote.NotesTestingUtils
+import com.lw.mynotes.featurenote.domain.model.Note
+import com.lw.mynotes.featurenote.domain.repository.NotesRepository
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class NotesServiceTest {
+
+    private val allNotesQtd = (11..99).random()
+    private val notesTestingUtils = NotesTestingUtils()
+
+    private lateinit var notesService: NotesService
+    @MockK
+    private lateinit var notesRepository: NotesRepository
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        notesService = NotesService(notesRepository)
+    }
+
+    // Basic functions/cases
+    @Test
+    fun getAllNotes(){
+        val mockAllNoteEntities = notesTestingUtils.createNoteEntities(allNotesQtd)
+        coEvery { notesRepository.getAll() } returns mockAllNoteEntities
+
+        val actualAllNoteEntities =  runBlocking {
+            notesService.getAll()
+        }
+
+        assert(actualAllNoteEntities.size == mockAllNoteEntities.size)
+    }
+
+    @Test
+    fun getById(){
+        val mockNoteEntity = notesTestingUtils.createNoteEntityWithId()
+        coEvery { notesRepository.get(mockNoteEntity.id) } returns mockNoteEntity
+
+        val actualNote = runBlocking {
+            notesService.getById(mockNoteEntity.id)
+        }
+
+        assert(actualNote == mockNoteEntity.toNote())
+    }
+
+    @Test
+    fun save_verify_result(){
+        val mockTitle = notesTestingUtils.getRandomTitle()
+        val mockContent = notesTestingUtils.getRandomContent()
+
+        val actualNote = notesService.create(mockTitle, mockContent)
+
+        assert(actualNote is Note)
+        assert(actualNote.title == mockTitle && actualNote.content == mockContent)
+    }
+
+    @Test
+    fun update(){
+
+    }
+
+    @Test
+    fun insertNote_ReturnsTrue() {
+
+
+    }
+
+}
